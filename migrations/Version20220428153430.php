8b31d6067a0fc1b2f23021673f54f13693cf7f72
alex <leaman00@mail.ru>
2022-04-28 22:39:43 +0700

    bbb

---
+++ migrations/Version20220428153430.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220428153430 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE news CHANGE date date DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE test ADD img VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE news CHANGE date date DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE test DROP img');
+    }
+}
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,14 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\Posts;
+use App\Entity\News;
+use App\Entity\Test;
 use App\Entity\User;
 use App\Form\UserType;
+use App\Form\PostType;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Repository\PostsRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -51,7 +56,6 @@ class DefaultController extends AbstractController
     public function post(PostsRepository $postsRepository, $id): Response
     {
         $post = $postsRepository->find($id);
-        //dd($post);
         return $this->render('default/currentPost.html.twig', [
             'post' => $post,
         ]);
@@ -81,35 +85,87 @@ class DefaultController extends AbstractController
     /**
      * @Route("/admin/post/add", name = "postAdd")
      */
-    public function postAdd(): Response
+    public function postAdd(Request $request, ManagerRegistry $doctrine, SluggerInterface $slugger): Response
     {
-        return $this->render('default/postAdd.html.twig');
+        $post = new Posts;
+
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // $form->getData() holds the submitted values
+            // но изначальная переменная `$task` также была обновлена
+            //$post = $form->getData();
+
+            $post->setHeader($form->get('header')->getData());
+            $post->setImg($form->get('img')->getData());
+            $post->setAnnotation($form->get('annotation')->getData());
+            $post->setDate($form->get('date')->getData());
+            $post->setFullText($form->get('fulltext')->getData());
+           
+            // $entityManager = $doctrine->getManager();
+
+            // // сообщите Doctrine, что вы хотите (в итоге) сохранить Продукт (пока без запросов)
+            // $entityManager->persist($post);
+            // // действительно выполните запросы (например, запрос INSERT)
+            // $entityManager->flush();
+            // ... выполните какое-то действие, например сохраните задачу в базу данных
+            // for example, if Task is a Doctrine entity, save it!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            $imgFile = $form->get('img')->getData();
+
+            if ($imgFile) {
+                $originalFilename = pathinfo($imgFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // это необходимо для безопасного включения имени файла в качестве части URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imgFile->guessExtension();
+
+                // Переместите файлв каталог, где хранятся брошюры
+                try {
+                    $imgFile->move(
+                        $this->getParameter('img_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... разберитесь с исключением, если что-то случится во время загрузки файла
+                }
+
+                // обновляет свойство 'brochureFilename' для сохранения имени PDF-файла,
+                // а не его содержания
+                $post->setImg($newFilename);
+                
+            }
+
+            return $this->redirectToRoute('posts');
+        }
+
+        return $this->renderForm('default/postAdd.html.twig', [
+            'form' => $form,
+        ]);
     }
 
     /**
-     * @Route("/admin/post/add/q", name = "postAddShow")
+     * @Route("/admin/news/add/q", name = "postAddShow")
      */
     public function postAddShow(ManagerRegistry $doctrine): Response
     {        
-        $post = new Posts();
+        $news = new News();
 
-        $post->setHeader('Третья новость');
-        $post->setImg('#');
-        $post->setAnnotation('Аннотация третьей новости');
-        $post->setDate(new DateTime('now'));
-        $post->setFullText('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.');
 
         $entityManager = $doctrine->getManager();
 
         // сообщите Doctrine, что вы хотите (в итоге) сохранить Продукт (пока без запросов)
-        $entityManager->persist($post);
+        $entityManager->persist($news);
 
         // действительно выполните запросы (например, запрос INSERT)
         $entityManager->flush();
 
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        return $this->render('default/index.html.twig');
     }
 
     #[Route(path: '/login', name: 'app_login')]
@@ -131,5 +187,41 @@ class DefaultController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/", name = "app_test", methods={"POST"})
+     */
+    public function test(Request $request)
+    {
+        dd($request);
+    }
+
+
+
+
+
+    /**
+     * @Route("/test", name = "test")
+     */
+    public function testic(ManagerRegistry $doctrine): Response
+    {
+        $test = new Test;
+
+        $test->setName('Firstname');
+        $test->setImg('#');
+        $test->setDate(new DateTime('now'));
+
+        $entityManager = $doctrine->getManager();
+
+        // сообщите Doctrine, что вы хотите (в итоге) сохранить Продукт (пока без запросов)
+        $entityManager->persist($test);
+
+        // действительно выполните запросы (например, запрос INSERT)
+        $entityManager->flush();
+
+        $test->setName('Firstname');
+
+        return new Response('Saved new product with id '.$test->getId());
     }
 }
